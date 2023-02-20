39a3f064bc60610e2735c32b8a723641dab644ea
BradMJ <90422753+BradMJ@users.noreply.github.com>
2023-02-20 11:49:03 -0800

    Add event listeners for div hover

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const rows = 16;
 const container = document.getElementById('container');
 const createDiv = document.createElement('div');
 
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
 const grid = document.createElement('div');
 grid.className = 'grid';
 for (let c = 0; c < columns; c++) {
@@ -13,6 +17,12 @@ for (let c = 0; c < columns; c++) {
         const row = document.createElement('div');
         row.className = 'row';
         row.textContent = ('c') + (c + 1) + '-' + ('r') + (r + 1);
+        row.addEventListener('mouseenter', () => {
+            row.style.backgroundColor = "rgb(" + randomColor() + "," + randomColor() + "," + randomColor() + ")";
+        });
+        row.addEventListener('mouseleave', () => {
+            row.style.backgroundColor = null;
+        });
         column.appendChild(row);
     }
     grid.appendChild(column);
