4f5a95044c4477ae3673ed9ec8c5762ba4c38c8e
BradMJ <90422753+BradMJ@users.noreply.github.com>
2023-02-27 08:20:14 -0800

    Add html button onclick events, add js color functions, board sizing and board reset functions

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-const container = document.getElementById('container');
+let color = 'black';
 
-function randomColor() {
-    return Math.floor(Math.random() * 256);
+// Color functions
+function colorSquare() {
+    if (color == 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
 }
 
+function changeColor(choice) {
+    color = choice;
+}
+
+// Board div generator
 function populateBoard(size) {
     const board = document.getElementById('board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < 256; i++) {
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        square.style.background = 'blue';
+        square.addEventListener('mouseover', colorSquare);
+        square.style.background = 'white';
         board.insertAdjacentElement('beforeend', square);
     }
 }
 
 populateBoard(16);
 
+function changeBoardSize(input) {
+    if (input >= 2 && input <= 100) {
+        document.querySelector('.errorText').style.display = 'none';
+        populateBoard(input); 
+    } else {
+        document.querySelector('.errorText').style.display = 'flex';
+    }
+}
+
+function resetBoard() {
+    const board = document.getElementById('board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
